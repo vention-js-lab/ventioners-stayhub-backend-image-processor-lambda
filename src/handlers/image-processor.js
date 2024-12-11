df9383a17b78f692c34f84970dd3b360c8257e11
purple-mountain <lmwrong92@gmail.com>
2024-12-11 18:09:08 +0000

    fix: added subnet and security group id

--- src/handlers/image-processor.js
+++ src/handlers/image-processor.js
@@ -3,22 +3,42 @@ const sharp = require("sharp");
 const { encode } = require("blurhash");
 const { Client } = require("pg");
 
+console.log("Start...");
+
 const s3 = new AWS.S3();
+
+console.log("Connecting to S3...");
+
 const cdnUrl = process.env.CDN_URL;
+const processedImagesBucketName = process.env.S3_PROCESSED_BUCKET_NAME;
+
+console.log("GETTING ENV VARIABLES");
 
 exports.handler = async (event) => {
+  console.log("STARTING HANDLER");
+
+  console.log("EVENT:", event);
+
   const dbClient = new Client({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
     port: process.env.DB_PORT,
+    ssl: {
+      require: true,
+      rejectUnauthorized: false,
+    },
   });
+
+  console.log("CONNECTING TO DB");
 
   try {
     const record = event.Records[0];
     const bucket = record.s3.bucket.name;
     const key = decodeURIComponent(record.s3.object.key.replace(/\+/g, " "));
+
+    console.log("S3 METADATA START:", record);
 
     const { Metadata } = await s3
       .headObject({
@@ -27,14 +47,20 @@ exports.handler = async (event) => {
       })
       .promise();
 
+    console.log("After getting s3 metadata:", Metadata);
+
     const accommodationId = Metadata["accommodation-id"];
     const uploadIndex = Metadata["upload-index"];
 
     console.log(`Processing file: ${key} from bucket: ${bucket}`);
 
+    console.log("Metadata:", Metadata);
+
     const { Body: imageBuffer } = await s3
       .getObject({ Bucket: bucket, Key: key })
       .promise();
+
+    console.log("Image buffer:", imageBuffer);
 
     const sharpInstance = sharp(imageBuffer);
 
@@ -47,8 +73,11 @@ exports.handler = async (event) => {
           .toBuffer(),
         sharpInstance
           .clone()
-          .resize({ width: 240, height: 427 })
-          .jpeg({ quality: 95 })
+          .resize({
+            width: 854,
+            height: 480,
+          })
+          .jpeg({ quality: 80 })
           .toBuffer(),
         sharpInstance
           .clone()
@@ -59,6 +88,8 @@ exports.handler = async (event) => {
       ],
     );
 
+    console.log("Image processed and resized");
+
     const blurhash = encode(
       new Uint8ClampedArray(data),
       info.width,
@@ -67,41 +98,51 @@ exports.handler = async (event) => {
       4,
     );
 
-    const fullSizeKey = `${key}-fullsize.jpg`;
-    const thumbnailKey = `${key}-thumbnail.jpg`;
+    const newKey = key.split("/")[1];
+
+    const fullSizeKey = `fullsize-${newKey}`;
+    const thumbnailKey = `thumbnail-${newKey}`;
+
+    console.log("Uploading images to S3...");
 
     await Promise.all([
       s3
         .putObject({
-          Bucket: bucket,
-          Key: fullSizeKey,
+          Bucket: processedImagesBucketName,
+          Key: `processed/${fullSizeKey}`,
           Body: fullSizeBuffer,
           ContentType: "image/jpeg",
         })
         .promise(),
       s3
         .putObject({
-          Bucket: bucket,
-          Key: thumbnailKey,
+          Bucket: processedImagesBucketName,
+          Key: `processed/${thumbnailKey}`,
           Body: thumbnailBuffer,
           ContentType: "image/jpeg",
         })
         .promise(),
     ]);
 
-    const fullSizeUrl = `${cdnUrl}/${fullSizeKey}`;
-    const thumbnailUrl = `${cdnUrl}/${thumbnailKey}`;
+    console.log("Upload complete");
+
+    const fullSizeUrl = `${cdnUrl}/processed/${fullSizeKey}`;
+    const thumbnailUrl = `${cdnUrl}/processed/${thumbnailKey}`;
+
+    console.log("Connecting to database...");
 
     await dbClient.connect();
 
+    console.log("Saving image to database...");
+
     const query = `
-      UPDATE images
+      UPDATE image
       SET
         url = $1,
-        thumbnail_url = $2,
+        "thumbnailUrl" = $2,
         blurhash = $3,
-        order = $4
-      WHERE accommodation_id = $5 AND order = $6
+        "order" = $4
+      WHERE "accommodationId" = $5 AND "order" = $6
     `;
 
     const values = [
@@ -114,6 +155,8 @@ exports.handler = async (event) => {
     ];
 
     const result = await dbClient.query(query, values);
+
+    console.log("Database update result:", result);
 
     if (result.rowCount === 0) {
       console.warn(
